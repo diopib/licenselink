3f91cc0fe99e368e2511c04bf7bc747fabd5e0c2
root <root@narbox>
2014-02-24 13:42:07 -0500

    added heroku settings

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -63,6 +63,8 @@ DATABASES = {
     }
 }
 
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
@@ -89,3 +91,10 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = '/static/'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+ 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
