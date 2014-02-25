ad55f2c5e109985900819d06fcc8cb74b9fbfdf3
Ibrahim Diop <ibrahim@zinaria.com>
2014-02-24 23:47:59 -0500

    debugging settings

--- licenselink/settings.py
+++ licenselink/settings.py
@@ -56,13 +56,14 @@ WSGI_APPLICATION = 'licenselink.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2', 'NAME': 'licenselink_db', 'HOST': 'localhost', 'USER': 'ibrahim', 'PASSWORD': 'blabla', 'PORT': None}}
 
 
 # Internationalization
@@ -92,9 +93,10 @@ STATICFILES_DIRS = (
 
 STATIC_URL = '/static/'
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
- 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# # Parse database configuration from $DATABASE_URL
+# import dj_database_url
+# DATABASES['default'] = dj_database_url.config()
+# print DATABASES
+#
+# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
